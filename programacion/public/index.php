f7228f8b9f1ab7be4d0bffefe2cbe7ead75d4420
JGHAlonso <111096506+JGHAlonso@users.noreply.github.com>
2022-09-15 18:49:20 -0600

    Add files via upload

---
+++ programacion/public/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="/programacion/public/css/main.css" rel="stylesheet" type="text/css" >
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+          <fieldset>
+            <form >
+            <legend>
+                acceso al panel
+            </legend>
+            <label >
+                correo electronico
+            </label>
+            <input type="email" name="">
+             <button>Acceder</button>
+            </form>
+          </fieldset>  
+    </div>
+</body>
+</html>
